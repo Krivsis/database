ebef97992461fb9cfaebcf3efc11625cdb1cbde2
Krivsis <103027148+Krivsis@users.noreply.github.com>
2022-11-15 15:59:50 +0800

    Update app.js

--- databaseServer/app.js
+++ databaseServer/app.js
@@ -1,12 +1,20 @@
+// 創造express伺服器
 var express=require('express');
+// 使用cors打開權限
 var cors=require("cors");
 var app=express();
+// http://localhost:8000/
 app.listen(8000);
+// 預設資料夾在public
 app.use( express.static('public') );
+// 可以使用json格式
 app.use( express.json() );
+// 開啟轉碼
 app.use( express.urlencoded( {extended: true}) );
+// 設定權限
 app.use(cors());
 
+// 設定資料庫來源
 var mysql=require('mysql');
 var conn=mysql.createConnection({
     user: "root",
@@ -15,7 +23,7 @@ var conn=mysql.createConnection({
     port: 3306,
     database: "database"
 })
-
+// 連接資料庫的時候判斷有沒有error
 conn.connect(function(err){
     if(err){
         console.log(err);
@@ -23,7 +31,7 @@ conn.connect(function(err){
         console.log("Server running")
     }
 })
-
+// 使用get創建路由
 app.get("/index", function (req, res) {
     conn.query("select * from item", [],
         function (err, result) {
